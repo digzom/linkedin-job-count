9a2008572258be1b66441124d92e953a4dd5c717
Dickson <dicksonmelo@gmail.com>
2022-09-27 15:39:32 -0300

    changing search words

--- lib/tech_count.ex
+++ lib/tech_count.ex
@@ -1,4 +1,62 @@
 defmodule TechCount do
+  @ci_cd ~w(
+    ci
+    cd
+    continuous
+    integration
+    integração
+    contínua
+    deploy
+    deployment
+    delivery
+  )
+  @database ~w(data banco bancos base database dado dados)
+  @relational ~w(relational relacional)
+  @ddd ~w(domain-driven ddd)
+  @tdd ~w(test-driven tdd)
+  @bdd ~w(behavior-driven bdd)
+  @test ~w(testing test teste testes tests)
+  @css ~w(css css3)
+  @data_structure_word_list ~w(
+    estruturas
+    dados
+    algoritmos
+    algoritmo
+    algorithms
+    algorithm
+    structure
+  )
+  @ingles_word_list ~w(ingles inglês)
+  @angular ~w(angular angularjs)
+  @distributed ~w(
+    distributed
+    distribuído
+    distribuídos
+  )
+  @react ~w(react reactjs)
+  @postgres_word_list ~w(
+    postgresql
+    postgre
+    postgres
+  )
+  @agile ~w(
+    agil
+    agile
+    agilidade
+  )
+  @rest ~w(rest restful)
+  @oo ~w(
+    objetos
+    objects
+    object
+    objeto
+    orientation
+    orientação
+    oop
+    poo
+    oo
+  )
+
   @spec count(binary) :: [tuple()]
   def count(path_to_file) do
     {:ok, content} = File.read(path_to_file)
@@ -49,6 +107,7 @@ defmodule TechCount do
     File.write!("./otherfile.txt", counting)
   end
 
-  # def group_by_relation(ordered_jobs) do
+  # def return_proper_group() do
+
   # end
 end
